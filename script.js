ab3add608590e5d51971e747bc89d2307d68c02b
Samuel Rodrigues <samuelrdl36@gmail.com>
2024-02-20 17:34:43 -0300

    menu responsivo

---
+++ script.js
@@ -0,0 +1,16 @@
+function menuShow(){
+let menuMobile =document.querySelector('.mobile-menu');
+if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "img/icons8-menu.svg";
+} else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "img/icons8-close.svg";
+
+
+
+}
+
+
+         }
+
